e44ba7d9263ffee874918bf7a6cce23efe903c92
carlosjulianodasilvabezerra <carlos_julianoti@outlook.com>
2020-10-24 21:43:15 -0300

    config bcryptjs hash

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true})
+mongoose.connect('mongodb://localhost/noderest', { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+mongoose.set('useCreateIndex', true)
+
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
